387fde0654dbdcd618ec9cdbc050e6da4eac1a24
Victor Lucas <royal.shop78@gmail.com>
2023-07-03 13:29:11 +0200

    nouvelle version function.php

--- wp-content/themes/chicdressing/functions.php
+++ wp-content/themes/chicdressing/functions.php
@@ -5,9 +5,8 @@ function chicdressing_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
 }
 
+
 add_filter( 'big_image_size_threshold', '__return_false' );
-
-
 
 
 function remove_google_fonts_stylesheet() {  
